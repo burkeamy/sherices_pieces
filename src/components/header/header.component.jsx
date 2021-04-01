ad8b5a4c1d6090833f2f6865f8407e41076f2c44
unknown <burkeamy30@gmail.com>
2021-04-01 19:20:52 -0400

    made design changes to the header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,21 +5,23 @@ import './header.styles.css'
 
 
 const Header = () => (
-    <div className = 'header'>
-          <Link className='logo-container' to='/'>
-            Logo Here
-        </Link>
-        <h1 className='page-title'>
+    <header >
+        <div className = 'header-container'>
+            <Link className='logo-container' to='/'>
+                Logo Here
+            </Link>
+            <h1 className='page-title'>
             Sherice's Pieces
-        </h1>
-        <div className='options'>
-            <Link className='option' to='/'>home |</Link>
-            <Link className='option' to='/homepage2'>Homepage Option 2 |</Link>
-            <Link className='option' to='/items'> items</Link>
-            <Link className='option' to='/about'> about us</Link>
-            <Link className='option' to='/faq'> faq</Link>
+            </h1>
+            <div className='options'>
+                <Link className='option' to='/'>home |</Link>
+                <Link className='option' to='/homepage2'>Homepage Option 2 |</Link>
+                <Link className='option' to='/items'> items</Link>
+                <Link className='option' to='/about'> about us</Link>
+                <Link className='option' to='/faq'> faq</Link>
+            </div>
         </div>
-    </div>
+    </header>
 );
 
 export default Header;
